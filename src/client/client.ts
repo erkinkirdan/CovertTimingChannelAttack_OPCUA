d271ab6777ce919ac5bc733a58f5c42020be3235
erkinkirdan <erkinkirdan@users.noreply.github.com>
2023-08-17 01:55:07 +0200

    Update client.ts

--- src/client/client.ts
+++ src/client/client.ts
@@ -19,25 +19,25 @@ const connectionStrategy = {
 
 const client = OPCUAClient.create({
     applicationName: "MyClient",
-    connectionStrategy: connectionStrategy,
+    connectionStrategy,
     securityMode: MessageSecurityMode.None,
     securityPolicy: SecurityPolicy.None,
     endpointMustExist: false
 });
 
-const endpointUrl = "opc.tcp://" + os.hostname() + ":4334/UA/MyLittleServer";
+const endpointUrl = `opc.tcp://${os.hostname()}:4334/UA/MyLittleServer`;
 
-async function timeout(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+async function timeout(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
     try {
         await client.connect(endpointUrl);
-        console.log("connected !");
+        console.log("Connected!");
 
         const session = await client.createSession();
-        console.log("session created !");
+        console.log("Session created!");
 
         const subscription = ClientSubscription.create(session, {
             requestedPublishingInterval: 1000,
@@ -48,13 +48,10 @@ async function main() {
             priority: 10
         });
 
-        subscription.on("started", () => {
-            console.log("subscription started - subscriptionId=", subscription.subscriptionId);
-        }).on("keepalive", () => {
-            console.log("keepalive");
-        }).on("terminated", () => {
-            console.log("terminated");
-        });
+        subscription
+            .on("started", () => console.log(`Subscription started - subscriptionId=${subscription.subscriptionId}`))
+            .on("keepalive", () => console.log("Keepalive"))
+            .on("terminated", () => console.log("Terminated"));
 
         const itemToMonitor: ReadValueIdOptions = {
             nodeId: "ns=1;s=my_node",
@@ -68,39 +65,37 @@ async function main() {
         };
 
         const monitoredItem = ClientMonitoredItem.create(subscription, itemToMonitor, parameters, TimestampsToReturn.Both);
-
         let lastChange = Date.now();
         let binaryString = "";
         const delimiter = "111111110";
 
         monitoredItem.on("changed", (dataValue: DataValue) => {
-            let delay = Date.now() - lastChange;
+            const delay = Date.now() - lastChange;
             lastChange = Date.now();
-            let bit = delay > 1500 ? '0' : '1';
+            const bit = delay > 1500 ? '0' : '1';
             console.log(`Observed delay: ${delay / 1000} seconds, decoded bit: ${bit}`);
             binaryString += bit;
             if (binaryString.endsWith(delimiter)) {
                 binaryString = binaryString.slice(0, -delimiter.length);
-                let charBits = binaryString.match(/.{1,8}/g);
+                const charBits = binaryString.match(/.{1,8}/g);
                 if (charBits) {
-                    let message = charBits.map(bits => String.fromCharCode(parseInt(bits, 2))).join('');
+                    const message = charBits.map(bits => String.fromCharCode(parseInt(bits, 2))).join('');
                     console.log(`Decoded message: ${message}`);
                     binaryString = "";
                 }
             }
         });
-        
-        await timeout(1000000)
 
-        console.log("now terminating subscription");
+        await timeout(1000000);
+        console.log("Now terminating subscription");
+
         await subscription.terminate();
-
         await session.close();
-
         await client.disconnect();
-        console.log("done !");
+
+        console.log("Done!");
     } catch (err) {
-        console.log("An error has occurred : ", err);
+        console.log("An error has occurred:", err);
     }
 }
 
